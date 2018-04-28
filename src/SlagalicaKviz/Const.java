16115264e87068591a577ef3185ac059ed66a969
Katarina Dimitrijevic <katjaa06@gmail.com>
2018-04-28 21:22:41 +0200

    test commit

--- src/SlagalicaKviz/Const.java
+++ src/SlagalicaKviz/Const.java
@@ -1,6 +1,5 @@
 package SlagalicaKviz;
 
-//test
 
 public class Const extends Node {
 }
--- src/SlagalicaKviz/Operacija.java
+++ src/SlagalicaKviz/Operacija.java
@@ -14,6 +14,7 @@ public class Operacija extends Node {
         PLUS, MINUS, MULTIPLY, DIVIDE
 
     }
+    //hwquhioq
 
     @Override
     public int calculate() {
